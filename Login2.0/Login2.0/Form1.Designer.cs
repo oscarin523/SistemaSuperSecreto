1cd29679918b571d3f8052edccc7fe7bbd80c0b8
Alumno <Alumno@PC23>
2019-08-26 09:20:09 -0500

    Adicion de Código

--- Login2.0/Login2.0/Form1.Designer.cs
+++ Login2.0/Login2.0/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.Salir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Super Secreto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario:";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(189, 239);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(225, 29);
+            this.textUser.TabIndex = 3;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(189, 315);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(225, 29);
+            this.textPassword.TabIndex = 4;
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(97, 488);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(135, 65);
+            this.Salir.TabIndex = 5;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 65);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Iniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(642, 594);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Login2.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Login2.0/Login2.0/Form1.cs
+++ Login2.0/Login2.0/Form1.cs
@@ -16,5 +16,30 @@ namespace Login2._0
         {
             InitializeComponent();
         }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            this.Close(); //Cierra el formulario
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(textUser.Text=="Juancho"&& textPassword.Text=="123tamarindo")
+            {
+                MessageBox.Show("Bienvenido Sr Don Juancho");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecto no Valido");
+                textUser.Text = "";
+                textPassword.Text = "";
+                textUser.Focus();
+            }
+        }
     }
 }
